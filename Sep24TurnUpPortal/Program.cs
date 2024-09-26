a5af372b9771ea8d79f000b17eff5ea622a4153e
kishanMax <kishannavaratne@gmail.com>
2024-09-26 22:23:51 +1200

    TurnUp Portal Login Test

---
+++ Sep24TurnUpPortal/Program.cs
@@ -0,0 +1,42 @@
+﻿
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Open Chrome Browser
+        IWebDriver driver = new ChromeDriver();
+
+        // Launch Turnup Portal
+        driver.Navigate().GoToUrl("http://horse.industryconnect.io/");
+        driver.Manage().Window.Maximize();
+
+        // Identify username textbox and enter valid username
+        IWebElement userNameTextbox = driver.FindElement(By.Id("UserName"));
+        userNameTextbox.SendKeys("hari");
+
+        // Identify password textbox and enter valid password
+        IWebElement passwordTextbox = driver.FindElement(By.Id("Password"));
+        passwordTextbox.SendKeys("123123");
+
+        // Identify login button and click on it
+        IWebElement loginButton = driver.FindElement(By.XPath("//*[@id=\"loginForm\"]/form/div[3]/input[1]"));
+        loginButton.Click();
+
+        // Check if user has logged in successfully
+        IWebElement helloHari = driver.FindElement(By.XPath("//*[@id=\"logoutForm\"]/ul/li/a"));
+
+        if (helloHari.Text == "Hello hari!")
+        {
+            Console.WriteLine("User has logged in successfully. Test Passed!");
+        }
+        else
+        {
+            Console.WriteLine("User has not logged in. Test failed!");
+        }
+
+    }
+}
